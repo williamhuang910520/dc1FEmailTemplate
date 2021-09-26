52507b390476779bdd0af54b61c56dd708da85fb
Hanchi Hunag <williamhuang910520@gmail.com>
2021-09-26 22:38:49 +0800

    加入專案檔案。

---
+++ dc1FEmailTemplate/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿
+namespace dc1FEmailTemplate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.startDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.name = new System.Windows.Forms.TextBox();
+            this.endDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ckb_late = new System.Windows.Forms.CheckBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.ckList_rentArea = new System.Windows.Forms.CheckedListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startDatePicker
+            // 
+            this.startDatePicker.Location = new System.Drawing.Point(337, 48);
+            this.startDatePicker.Margin = new System.Windows.Forms.Padding(2);
+            this.startDatePicker.Name = "startDatePicker";
+            this.startDatePicker.Size = new System.Drawing.Size(106, 22);
+            this.startDatePicker.TabIndex = 0;
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(337, 14);
+            this.name.Margin = new System.Windows.Forms.Padding(2);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(106, 22);
+            this.name.TabIndex = 1;
+            // 
+            // endDatePicker
+            // 
+            this.endDatePicker.Location = new System.Drawing.Point(337, 82);
+            this.endDatePicker.Margin = new System.Windows.Forms.Padding(2);
+            this.endDatePicker.Name = "endDatePicker";
+            this.endDatePicker.Size = new System.Drawing.Size(106, 22);
+            this.endDatePicker.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(280, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "起始日期";
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(284, 326);
+            this.buttonGenerate.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(159, 30);
+            this.buttonGenerate.TabIndex = 3;
+            this.buttonGenerate.Text = "生成文字";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "結束日期";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 21);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "姓氏";
+            // 
+            // ckb_late
+            // 
+            this.ckb_late.AutoSize = true;
+            this.ckb_late.Location = new System.Drawing.Point(337, 272);
+            this.ckb_late.Margin = new System.Windows.Forms.Padding(2);
+            this.ckb_late.Name = "ckb_late";
+            this.ckb_late.Size = new System.Drawing.Size(72, 16);
+            this.ckb_late.TabIndex = 4;
+            this.ckb_late.Text = "是否逾期";
+            this.ckb_late.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(14, 17);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(248, 292);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "按下生成文字以產生內容...";
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(15, 326);
+            this.buttonCopy.Margin = new System.Windows.Forms.Padding(2);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(246, 30);
+            this.buttonCopy.TabIndex = 6;
+            this.buttonCopy.Text = "複製文字";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // ckList_rentArea
+            // 
+            this.ckList_rentArea.FormattingEnabled = true;
+            this.ckList_rentArea.Items.AddRange(new object[] {
+            "A區",
+            "B區",
+            "C區",
+            "D區",
+            "E區",
+            "F區",
+            "草皮",
+            "大廣場"});
+            this.ckList_rentArea.Location = new System.Drawing.Point(337, 119);
+            this.ckList_rentArea.Margin = new System.Windows.Forms.Padding(2);
+            this.ckList_rentArea.Name = "ckList_rentArea";
+            this.ckList_rentArea.Size = new System.Drawing.Size(106, 140);
+            this.ckList_rentArea.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 122);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "借用區域";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 369);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ckList_rentArea);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ckb_late);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.endDatePicker);
+            this.Controls.Add(this.startDatePicker);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "院辦Email催繳";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker startDatePicker;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.DateTimePicker endDatePicker;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox ckb_late;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button buttonCopy;
+        private System.Windows.Forms.CheckedListBox ckList_rentArea;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ dc1FEmailTemplate/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+
+
+namespace dc1FEmailTemplate
+{
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            String filePath = "\\\\140.125.155.18\\DataCenter\\110學年\\01-大廳活動\\自訂催繳格式.rtf";
+            String debugPath = "C:\\Users\\Hanchi\\Desktop\\MyDocument.rtf"
+;            if (name.Text == "")
+            {
+                MessageBox.Show("姓氏為空");
+                return;
+            }
+            if (ckList_rentArea.CheckedItems.Count == 0)
+            {
+                MessageBox.Show("沒有選擇場地");
+                return;
+            }
+
+            richTextBox1.LoadFile(filePath);
+
+            richTextBox1.Find("*姓氏*", RichTextBoxFinds.MatchCase);
+            richTextBox1.SelectedText = name.Text;
+
+            richTextBox1.Find("*開始日期*", RichTextBoxFinds.MatchCase);
+            int minYear = int.Parse(startDatePicker.Value.ToString("yyyy"))-1911;
+            richTextBox1.SelectedText = minYear.ToString() + startDatePicker.Value.ToString("/MM/dd");
+
+            richTextBox1.Find("*結束日期*", RichTextBoxFinds.MatchCase);
+            minYear = int.Parse(startDatePicker.Value.ToString("yyyy")) - 1911;
+            richTextBox1.SelectedText = minYear.ToString() + endDatePicker.Value.ToString("/MM/dd");
+
+            richTextBox1.Find("*借用場地*", RichTextBoxFinds.MatchCase);
+            string s = ckList_rentArea.CheckedItems[0].ToString();
+            for (int x = 1; x < ckList_rentArea.CheckedItems.Count; x++)
+            {
+                if(x == ckList_rentArea.CheckedItems.Count - 1)
+                {
+                    s += "及" + ckList_rentArea.CheckedItems[x].ToString();
+                    break;
+                }
+                s += "、" + ckList_rentArea.CheckedItems[x].ToString();
+            }
+            richTextBox1.SelectedText = s;
+
+            richTextBox1.Find("*是否逾時*", RichTextBoxFinds.MatchCase);
+            richTextBox1.SelectedText = ckb_late.Checked?"由於逾時過久，所以":"";
+
+            richTextBox1.Find("*歸還日期*", RichTextBoxFinds.MatchCase);
+            DateTime date = endDatePicker.Value.AddDays(5);
+            minYear = int.Parse(date.ToString("yyyy")) - 1911;
+            richTextBox1.SelectedText = minYear.ToString() + date.ToString("/MM/dd");
+
+
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        {
+            // Clipboard.SetText(richTextBox1.Rtf, TextDataFormat.Rtf);
+            outlookSendMail();
+        }
+
+        public void SendEmail()
+        {
+            //設定smtp主機
+            string smtpAddress = "smtp.gmail.com";
+            //設定Port
+            int portNumber = 587;
+            bool enableSSL = true;
+            //填入寄送方email和密碼
+            string emailFrom = "hanchitune76@gmail.com";
+            string password = "hanchi_Tune";
+            //收信方email
+            string emailTo = "b10917013@gemail.yuntech.edu.tw";
+            //主旨
+            string subject = "Hello";
+            //內容
+            string body = "Hello, I'm just writing this to say Hi!";
+
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(emailFrom);
+                mail.To.Add(emailTo);
+                mail.Subject = subject;
+                mail.Body = body;
+                // 若你的內容是HTML格式，則為True
+                mail.IsBodyHtml = false;
+
+                //夾帶檔案
+                mail.Attachments.Add(new Attachment("C:\\Users\\Hanchi\\Desktop\\MyDocument.rtf"));
+
+                using (SmtpClient smtp = new SmtpClient(smtpAddress, portNumber))
+                {
+                    smtp.Credentials = new NetworkCredential(emailFrom, password);
+                    smtp.EnableSsl = enableSSL;
+                    smtp.Send(mail);
+                }
+            }
+        }
+
+        private void outlookSendMail()
+        {
+
+        }
+    }
+}
